fb9e0d5eeedfaecca3b679844360e66c91a379ba
DenisMPar <paradadenismartin@gmail.com>
2022-11-18 20:43:53 -0300

    added head title

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,14 @@ import { ContactSection } from "../components/contactSection";
 import { HomeSection } from "../components/homeSection";
 import { Header } from "../components/header/Header";
 import { Footer } from "../components/footer/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title> Split - Materiales de Atado</title>
+      </Head>
       <Header />
       <HomeSection />
       <AboutUsSection />
